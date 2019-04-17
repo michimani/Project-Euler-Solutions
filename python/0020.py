f5a673bb8ded13e4ca2aff6ec0c2262cdb5645f2
michimani <michimani210@gmail.com>
2019-04-17 17:56:48 +0900

    add solution for problem 20

---
+++ python/0020.py
@@ -0,0 +1,22 @@
+#
+# Solution for Project Euler problem 20
+# Copyright michimani All rights reserved.
+#
+# https://projecteuler.net/problem=20
+#
+
+
+def compute():
+    ans = 0
+    times = 1
+    for n in range(1, 101):
+        times = times * n
+
+    for digit in list(str(times)):
+        ans = ans + int(digit)
+
+    return ans
+
+
+if __name__ == "__main__":
+    print(compute())
