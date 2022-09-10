5a9637659c3220fef924c45b634fad8c307b59d7
michimani <michimani210@gmail.com>
2022-09-10 22:28:49 +0900

    [Go] improve a solution for problem 10

--- go/solution/0010.go
+++ go/solution/0010.go
@@ -1,31 +1,28 @@
-//
 // Solution for Project Euler problem 10
 // Copyright michimani All rights reserved.
 //
 // https://projecteuler.net/problem=10
-//
 package solution
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 // Solve0010 is function to solve the problem.
 func Solve0010() {
 	var answer int
-	var primes []int = []int{}
-	var isPrime bool = true
 
 	for num := 2; num < 2000000; num++ {
-		isPrime = true
-
-		for _, prime := range primes {
-			if num%prime == 0 {
+		isPrime := true
+		for d := 2; d < int(math.Sqrt(float64(num))+1); d++ {
+			if num%d == 0 {
 				isPrime = false
 				break
 			}
 		}
 
 		if isPrime {
-			primes = append(primes, num)
 			answer = answer + num
 		}
 	}
