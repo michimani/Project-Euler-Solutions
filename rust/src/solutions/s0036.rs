57fdac1b533e24ddededc73604d5e3bd229c9aff
michimani <michimani210@gmail.com>
2022-09-24 14:57:10 +0900

    [Rust] replace is_palindromic function to utils one at solution 36

--- rust/src/solutions/s0036.rs
+++ rust/src/solutions/s0036.rs
@@ -1,3 +1,4 @@
+use crate::utils;
 use proconio::input;
 use std::time::Instant;
 
@@ -18,7 +19,8 @@ pub fn solve() {
     let mut answer = 0;
 
     for num in 1..n + 1 {
-        if is_palindromic(format!("{:b}", num).as_str()) && is_palindromic(num.to_string().as_str())
+        if utils::sequence::is_palindromic(format!("{:b}", num).as_str())
+            && utils::sequence::is_palindromic(num.to_string().as_str())
         {
             answer += num;
         }
@@ -32,28 +34,4 @@ pub fn solve() {
         end.as_secs(),
         end.subsec_nanos() / 1_000_000
     );
-}
-
-/// Check the string is palindromic.
-fn is_palindromic(num_str: &str) -> bool {
-    let mid = num_str.len() / 2;
-    let before_mid = &num_str[0..mid];
-    let after_mid = &num_str[mid..num_str.len()];
-
-    let mut is_palindromic = true;
-    for i in 0..before_mid.len() {
-        if before_mid[i..i + 1] != after_mid[after_mid.len() - i - 1..after_mid.len() - i] {
-            is_palindromic = false;
-            break;
-        }
-    }
-
-    return is_palindromic;
-}
-
-#[test]
-fn test_is_palindromic() {
-    assert_eq!(true, is_palindromic("100101001"));
-    assert_eq!(true, is_palindromic("1001001001"));
-    assert_eq!(false, is_palindromic("1001001000"));
 }
