e89e7fc36906a17e49da2ae936145a0337101b69
michimani <michimani210@gmail.com>
2022-09-27 22:30:51 +0900

    [Rust] add is_hexagonal function to utils

--- rust/src/utils/natural.rs
+++ rust/src/utils/natural.rs
@@ -46,3 +46,31 @@ fn test_is_pentagonal_number() {
     assert_eq!(true, is_pentagonal_number(40755));
     assert_eq!(false, is_pentagonal_number(9999));
 }
+
+/// Check the number is a hexagonal number.
+///
+/// Example of hexagonal numbers: 1, 6, 15, 28, 45...
+pub fn is_hexagonal_number(n: i64) -> bool {
+    let mut check = n;
+    let mut count = 0;
+    return loop {
+        if check == 0 {
+            break true;
+        }
+
+        if check < 0 {
+            break false;
+        }
+
+        check = check - 4 * count - 1;
+        count += 1;
+    };
+}
+
+#[test]
+fn test_is_hexagonal_number() {
+    assert_eq!(true, is_hexagonal_number(1));
+    assert_eq!(true, is_hexagonal_number(28));
+    assert_eq!(true, is_hexagonal_number(40755));
+    assert_eq!(false, is_hexagonal_number(9999));
+}
