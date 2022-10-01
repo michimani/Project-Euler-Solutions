2b223d3d3958398006373524910597262b11a77b
michimani <michimani210@gmail.com>
2022-10-01 11:41:34 +0900

    [Rust] fix functions in utils::big

--- rust/src/utils/big.rs
+++ rust/src/utils/big.rs
@@ -1,8 +1,6 @@
-use std::io;
-use std::num::ParseIntError;
 use std::result::Result;
 
-pub fn sum_of(na: Vec<usize>, nb: Vec<usize>) -> Vec<usize> {
+pub fn sum_of(na: &Vec<usize>, nb: &Vec<usize>) -> Vec<usize> {
     let mut ans = na.clone();
     let mut dc = nb.len();
     if &nb.len() > &ans.len() {
@@ -35,22 +33,22 @@ pub fn sum_of(na: Vec<usize>, nb: Vec<usize>) -> Vec<usize> {
 
 #[test]
 fn test_sum_of() {
-    assert_eq!([4, 1].to_vec(), sum_of([0, 1].to_vec(), [4].to_vec()));
+    assert_eq!([4, 1].to_vec(), sum_of(&[0, 1].to_vec(), &[4].to_vec()));
     assert_eq!(
         [1, 3, 2, 1].to_vec(),
-        sum_of([0, 1, 0, 1].to_vec(), [1, 2, 2].to_vec())
+        sum_of(&[0, 1, 0, 1].to_vec(), &[1, 2, 2].to_vec())
     );
     assert_eq!(
         [1, 3, 2, 1].to_vec(),
-        sum_of([1, 2].to_vec(), [0, 1, 2, 1].to_vec())
+        sum_of(&[1, 2].to_vec(), &[0, 1, 2, 1].to_vec())
     );
     assert_eq!(
         [1, 3, 2, 1].to_vec(),
-        sum_of([0, 1, 2, 1].to_vec(), [1, 2].to_vec())
+        sum_of(&[0, 1, 2, 1].to_vec(), &[1, 2].to_vec())
     );
 }
 
-pub fn product_of(na: Vec<usize>, nb: Vec<usize>) -> Vec<usize> {
+pub fn product_of(na: &Vec<usize>, nb: &Vec<usize>) -> Vec<usize> {
     let mut ans = Vec::new();
 
     for (i, a) in na.iter().enumerate() {
@@ -60,7 +58,7 @@ pub fn product_of(na: Vec<usize>, nb: Vec<usize>) -> Vec<usize> {
         }
 
         let mut add_next = 0;
-        for b in &nb {
+        for b in nb {
             let mut t = a * b + add_next;
             add_next = 0;
             if t > 9 {
@@ -75,7 +73,7 @@ pub fn product_of(na: Vec<usize>, nb: Vec<usize>) -> Vec<usize> {
             ans_tmp.push(add_next);
         }
 
-        ans = sum_of(ans, ans_tmp);
+        ans = sum_of(&ans, &ans_tmp);
     }
 
     return ans;
@@ -85,21 +83,21 @@ pub fn product_of(na: Vec<usize>, nb: Vec<usize>) -> Vec<usize> {
 fn test_product_of() {
     assert_eq!(
         [1, 2, 1].to_vec(),
-        product_of([1, 1].to_vec(), [1, 1].to_vec())
+        product_of(&[1, 1].to_vec(), &[1, 1].to_vec())
     );
     assert_eq!(
         [0, 5, 8, 5, 5, 6, 7, 9, 0, 2, 3, 9, 1, 2, 1].to_vec(),
         product_of(
-            [5, 6, 7, 8, 9].to_vec(),
-            [0, 9, 8, 7, 6, 5, 4, 3, 2, 1].to_vec()
+            &[5, 6, 7, 8, 9].to_vec(),
+            &[0, 9, 8, 7, 6, 5, 4, 3, 2, 1].to_vec()
         )
     );
 }
 
-pub fn to_string(big: Vec<usize>) -> String {
+pub fn to_string(big: &Vec<usize>) -> String {
     let mut s = String::new();
 
-    let mut tmp = big;
+    let mut tmp = big.clone();
     tmp.reverse();
     for t in tmp {
         s = format!("{}{}", s, t);
@@ -110,7 +108,7 @@ pub fn to_string(big: Vec<usize>) -> String {
 
 #[test]
 fn test_to_string() {
-    assert_eq!("12345", to_string([5, 4, 3, 2, 1].to_vec()))
+    assert_eq!("12345", to_string(&[5, 4, 3, 2, 1].to_vec()))
 }
 
 pub fn to_big(num_str: &str) -> Result<Vec<usize>, &str> {
