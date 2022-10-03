fdf75ac30a868f7b57dcfa45248ff95ca0c820fc
michimani <michimani210@gmail.com>
2022-10-03 23:16:57 +0900

    [Rust] fix test (util::big)

--- rust/src/utils/big.rs
+++ rust/src/utils/big.rs
@@ -31,23 +31,6 @@ pub fn sum_of(na: &Vec<usize>, nb: &Vec<usize>) -> Vec<usize> {
     return ans;
 }
 
-#[test]
-fn test_sum_of() {
-    assert_eq!([4, 1].to_vec(), sum_of(&[0, 1].to_vec(), &[4].to_vec()));
-    assert_eq!(
-        [1, 3, 2, 1].to_vec(),
-        sum_of(&[0, 1, 0, 1].to_vec(), &[1, 2, 2].to_vec())
-    );
-    assert_eq!(
-        [1, 3, 2, 1].to_vec(),
-        sum_of(&[1, 2].to_vec(), &[0, 1, 2, 1].to_vec())
-    );
-    assert_eq!(
-        [1, 3, 2, 1].to_vec(),
-        sum_of(&[0, 1, 2, 1].to_vec(), &[1, 2].to_vec())
-    );
-}
-
 pub fn product_of(na: &Vec<usize>, nb: &Vec<usize>) -> Vec<usize> {
     let mut ans = Vec::new();
 
@@ -79,21 +62,6 @@ pub fn product_of(na: &Vec<usize>, nb: &Vec<usize>) -> Vec<usize> {
     return ans;
 }
 
-#[test]
-fn test_product_of() {
-    assert_eq!(
-        [1, 2, 1].to_vec(),
-        product_of(&[1, 1].to_vec(), &[1, 1].to_vec())
-    );
-    assert_eq!(
-        [0, 5, 8, 5, 5, 6, 7, 9, 0, 2, 3, 9, 1, 2, 1].to_vec(),
-        product_of(
-            &[5, 6, 7, 8, 9].to_vec(),
-            &[0, 9, 8, 7, 6, 5, 4, 3, 2, 1].to_vec()
-        )
-    );
-}
-
 pub fn to_string(big: &Vec<usize>) -> String {
     let mut s = String::new();
 
@@ -104,11 +72,6 @@ pub fn to_string(big: &Vec<usize>) -> String {
     }
 
     return s;
-}
-
-#[test]
-fn test_to_string() {
-    assert_eq!("12345", to_string(&[5, 4, 3, 2, 1].to_vec()))
 }
 
 pub fn to_big(num_str: &str) -> Result<Vec<usize>, &str> {
@@ -126,10 +89,53 @@ pub fn to_big(num_str: &str) -> Result<Vec<usize>, &str> {
     return Ok(big);
 }
 
-#[test]
-fn test_to_big() {
-    let r1 = to_big("12345");
-    assert_eq!(Ok([5, 4, 3, 2, 1].to_vec()), r1);
-    let r2 = to_big("12345abc");
-    assert!(r2.is_err());
+#[cfg(test)]
+
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_sum_of() {
+        assert_eq!([4, 1].to_vec(), sum_of(&[0, 1].to_vec(), &[4].to_vec()));
+        assert_eq!(
+            [1, 3, 2, 1].to_vec(),
+            sum_of(&[0, 1, 0, 1].to_vec(), &[1, 2, 2].to_vec())
+        );
+        assert_eq!(
+            [1, 3, 2, 1].to_vec(),
+            sum_of(&[1, 2].to_vec(), &[0, 1, 2, 1].to_vec())
+        );
+        assert_eq!(
+            [1, 3, 2, 1].to_vec(),
+            sum_of(&[0, 1, 2, 1].to_vec(), &[1, 2].to_vec())
+        );
+    }
+
+    #[test]
+    fn test_product_of() {
+        assert_eq!(
+            [1, 2, 1].to_vec(),
+            product_of(&[1, 1].to_vec(), &[1, 1].to_vec())
+        );
+        assert_eq!(
+            [0, 5, 8, 5, 5, 6, 7, 9, 0, 2, 3, 9, 1, 2, 1].to_vec(),
+            product_of(
+                &[5, 6, 7, 8, 9].to_vec(),
+                &[0, 9, 8, 7, 6, 5, 4, 3, 2, 1].to_vec()
+            )
+        );
+    }
+
+    #[test]
+    fn test_to_string() {
+        assert_eq!("12345", to_string(&[5, 4, 3, 2, 1].to_vec()))
+    }
+
+    #[test]
+    fn test_to_big() {
+        let r1 = to_big("12345");
+        assert_eq!(Ok([5, 4, 3, 2, 1].to_vec()), r1);
+        let r2 = to_big("12345abc");
+        assert!(r2.is_err());
+    }
 }
